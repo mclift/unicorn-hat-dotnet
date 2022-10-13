67cc04dcb6fdaff8a8e9bda9fd265e9c075d3cf3
Mike Clift <cliftm@rsc.org>
2022-10-13 10:35:00 +0100

    Add HSV conversion helper

---
+++ src/UnicornHatHdDotnet/ColorHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+
+namespace Mclift.UnicornHatHdDotnet
+{
+    public static class ColorHelper
+    {
+        public static Color ColorFromHsv(double hue, double saturation, double value)
+        {
+            // See: https://stackoverflow.com/questions/1335426
+            var hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            var f = (hue / 60) - Math.Floor(hue / 60);
+
+            value = value * 255;
+            var v = Convert.ToInt32(value);
+            var p = Convert.ToInt32(value * (1 - saturation));
+            var q = Convert.ToInt32(value * (1 - f * saturation));
+            var t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+
+            if (hi == 0)
+                return Color.FromArgb(255, v, t, p);
+            else if (hi == 1)
+                return Color.FromArgb(255, q, v, p);
+            else if (hi == 2)
+                return Color.FromArgb(255, p, v, t);
+            else if (hi == 3)
+                return Color.FromArgb(255, p, q, v);
+            else if (hi == 4)
+                return Color.FromArgb(255, t, p, v);
+            else
+                return Color.FromArgb(255, v, p, q);
+        }
+    }
+}
